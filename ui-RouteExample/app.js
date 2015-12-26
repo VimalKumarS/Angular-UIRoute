adca032396dc38cc32580a6974566d6de1acdc15
vimalkumarsinghal <vimalkumarsinghal@gmail.com>
2015-12-26 10:48:26 -0800

    first commit

---
+++ ui-RouteExample/app.js
@@ -0,0 +1,67 @@
+/**
+ * Created by vimalkumar on 12/26/2015.
+ */
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+
+
+        // nested list with custom controller
+        .state('home.list', {
+            url: '/list',
+            templateUrl: 'partial-home-list.html',
+            controller: function($scope) {
+                $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
+            }
+        })
+
+        // nested list with just some random string data
+        .state('home.paragraph', {
+            url: '/paragraph',
+            template: 'I could sure use a drink right now.'
+        })
+        .state('about',{
+           url:'/about',
+            views:{
+                '':{templateUrl: 'partial-about.html'},
+                "columnOne@about" :{template:'Look i am a column one'},
+                'columnTwo@about':{
+                    templateUrl:'table-data.html',
+                    controller: 'scotchController'
+                }
+            }
+
+        });
+
+});
+
+routerApp.controller('scotchController', function($scope) {
+
+    $scope.message = 'test';
+
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
+        }
+    ];
+
+});
